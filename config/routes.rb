da79b0463a003f22148590f77c1edc00ce37273c
Dandush03 <d.laloush@outlook.com>
2020-03-16 17:35:05 -0400

    added controler

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   get 'users/new', to: 'users#new', as: 'users'
   post 'users/new', to: 'users#create'
   get 'users/show'
