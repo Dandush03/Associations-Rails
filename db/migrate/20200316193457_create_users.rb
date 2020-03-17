3fd11073dbceb415551dc4866f7a4efb4afb6395
Dandush03 <d.laloush@outlook.com>
2020-03-17 15:40:39 -0400

    Fixed Schema

--- db/migrate/20200316193457_create_users.rb
+++ db/migrate/20200316193457_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :name
-
+      t.string :token
       t.timestamps
     end
   end
--- db/migrate/20200316194217_add_token_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddTokenToUser < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :token, :string
-  end
-end
