82758893d82fc0a14d0481071de7bf852d89469b
Dandush03 <d.laloush@outlook.com>
2020-03-17 18:54:24 -0400

    added a redirect route

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,12 @@
 class EventsController < ApplicationController
+  include SessionsHelper
   def new
+    redirect_to root_path if current_user.nil?
     @event = Event.new
   end
 
   def create
-    
+
   end
 
   def destroy
