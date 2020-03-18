c2739721073dadca997936ab3363f87721df9c32
Dandush03 <d.laloush@outlook.com>
2020-03-18 18:35:49 -0400

    User Display Attending Events

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,4 +2,8 @@ module EventsHelper
 	def user_attending?
 		!Creator.find_by(:user_id => current_user.id, :event_id => @event.id).nil?
 	end
+
+	def event(attend)
+		@event = attend.event
+	end
 end
